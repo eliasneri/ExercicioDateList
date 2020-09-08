3362492484d6f3d4f38e6df376aeeaae61634404
eliasneri <eliasneri@hotmail.com>
2020-09-08 17:53:41 -0300

    Começando a Implementar

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Exercícios Datas e Listas");
+		
+		Scanner sc = new Scanner(System.in);
+		List<String> list = new ArrayList<>();
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date data = new Date();
+		
+				
+		
+		
+		sc.close();
 
 	}
 
